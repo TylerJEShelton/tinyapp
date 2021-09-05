e8629e6fd62782fa5c153338b38d218c971e3a7d
Tyler <tylershelton99@gmail.com>
2021-09-05 19:06:33 +0000

    Clean up code, add comments, add screenshots for future README

--- express_server.js
+++ express_server.js
@@ -1,12 +1,19 @@
+// Program Name: TinyApp
+// Written By: Tyler Shelton
+// Started On: August 28, 2021
+// Description: This program allows a user to login/register as a user and create, store and share short URLs for longer URLs 
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+// Requires and constants
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const cookieSession = require("cookie-session");
 const { generateRandomString, lookupUserByEmail, urlsForUser } = require("./helpers");
+const PORT = 8080; // default port 8080
+const saltRounds = 12;  // default saltRounds for bcrypt
 
 const app = express();
-const PORT = 8080; // default port 8080
-const saltRounds = 12;
 
 // URL Database that stores the shortURLS as the key with each one containing an object w/ longURL and the userID of who created the shortURL
 const urlDatabase = {
@@ -35,6 +42,8 @@ const users = {
   }
 };
 
+// Middleware
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(cookieSession({
@@ -42,131 +51,153 @@ app.use(cookieSession({
   keys: ['key1', 'key2']
 }));
 
+// POST Endpoints
+
+// creates a new short url for logged in users and send them to the show URL page
 app.post("/urls", (req, res) => {
+  // generates a random 6 letter string to be the shortURL
   const newShortURL = generateRandomString();
   const userID = req.session.user_id;
-  urlDatabase[newShortURL] = { "longURL": req.body.longURL };
+  const longURL = req.body.longURL;
+  urlDatabase[newShortURL] = { "longURL": longURL };
   urlDatabase[newShortURL]["userID"] = userID;
   const templateVars = {
     user: users[userID],
     shortURL: newShortURL,
-    longURL: req.body.longURL
+    longURL: longURL
   };
   res.render("urls_show", templateVars);
 });
 
+// registers a new user
 app.post("/register", (req, res) => {
+  // generates a random 6 letter string to be the shortURL
   const newUserID = generateRandomString();
   const email = req.body.email;
   const password = req.body.password;
-
+  // if the email or password fields are left blank, send the user to the error page
   if (!email || !password) {
     const templateVars = {
-      user: users[req.session.user_id],
-      error: "The email and/or password entered are blank.  These fields must contain values and can't be left blank."
+      user: users[null],
+      error: "400 - The email and/or password entered are blank.  These fields must contain values and cannot be left blank."
     };
     return res.status(400).render("error", templateVars);
-    //return res.status(400).send("The email and/or password entered are blank.  These fields must contain values and can't be left blank.");
   }
+  // if the email address entered is associated with a user that is already registered, send them to the error page
   if (lookupUserByEmail(email, users)) {
     const templateVars = {
-      user: users[req.session.user_id],
-      error: "This email is already associated with a user.  Please register with a new email address or login with the email you provided."
+      user: users[null],
+      error: "400 - This email is already associated with a user.  Please register with a new email address or login with the email you provided."
     };
     return res.status(400).render("error", templateVars);
-    //return res.status(400).send("This email is already associated with a user.  Please register with a new email address or login with the email you provided.");
   }
-
+  // if the email and password entered are valid, hash the password and create the new user
   const hashedPassword = bcrypt.hashSync(password, saltRounds);
   users[newUserID] = {
     id: newUserID,
     email: email,
     password: hashedPassword
   };
+  // set the newUserID to the cookie and redirect to the index page
   req.session.user_id = newUserID;
   res.redirect("/urls");
 });
 
+// deletes the selected short URL from the database
 app.post("/urls/:shortURL/delete", (req, res) => {
+  // if a shortURL delete request is submitted by an unauthorized user, send them to the error page 
   if (urlDatabase[req.params.shortURL].userID !== req.session.user_id) {
-    //return res.status(403).send("This is not your link!  Please sign in to the proper account to access this link!");
     const templateVars = {
       user: users[req.session.user_id],
-      error: "This is not your link!  Please sign in to the proper account to access this link!"
+      error: "403 - Unauthorized access to this link!  You cannot delete this link! Please sign in to the proper account to access this link!"
     };
-    return res.status(400).render("error", templateVars);
+    return res.status(403).render("error", templateVars);
   }
+  // if the owner of the shortURL requests to delete the url, allow it and redirect to the index page
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
+// logs the user into the account information they provide
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const userID = lookupUserByEmail(email, users);
-
+  // if either the email or password fields are left blank, send the user to the error page
   if (!email || !password) {
     const templateVars = {
       user: users[userID],
-      error: "The email and/or password entered are blank.  These fields must contain values and can't be left blank."
+      error: "400 - The email and/or password entered are blank.  These fields must contain values and can't be left blank."
     };
     return res.status(400).render("error", templateVars);
-
-    //return res.status(400).send("The email and/or password entered are blank.  These fields must contain values and can't be left blank.");
   }
+  // check to see if the email address is connected to a current user in the database and return the userID
+  const userID = lookupUserByEmail(email, users);
+  // if the user does not currently exist in the database, send them to the error page
   if (!userID) {
     const templateVars = {
       user: users[userID],
-      error: "This email does not have an account registered.  Please register with this email address or login with a different email address that is already associated with an account."
+      error: "400 - This email does not have an account registered.  Please register with this email address or login with a different email address that is already associated with an account."
     };
     return res.status(400).render("error", templateVars);
-    //return res.status(400).send("This email does not have an account registered.  Please register with this email address or login with a different email address that is already associated with an account.");
   }
+  // if the password the user tried does not match the stored password for the email provided, send the user to the error page
   if (!bcrypt.compareSync(password, users[userID].password)) {
     const templateVars = {
       user: users[null],
-      error: "You have entered the incorrect password.  Please try again."
+      error: "403 - You have entered the incorrect password.  Please try again."
     };
     return res.status(403).render("error", templateVars);
-    //return res.status(403).send("You have entered the incorrect password.  Please try again.");
   }
-
+  // if the user exists, and logs in successfully with the proper credentials, set the cookie and send them to the index page
   req.session.user_id = userID;
   res.redirect("/urls");
 });
 
+// Logs the user out when the logout button is pressed and destroys the cookie
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/urls");
 });
 
+// edits the current long URL for the associated short URL 
 app.post("/urls/:shortURL", (req, res) => {
   const userID = req.session.user_id;
-  urlDatabase[req.params.shortURL] = { "longURL": req.body.longURL };
-  urlDatabase[req.params.shortURL]["userID"] = userID;
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  // if the short URL does not exist, send the user to the error page
+  if (!urlDatabase[req.params.shortURL]) {
+    const templateVars = {
+      user: users[userID],
+      error: "400 - This is not a valid link!"
+    };
+    return res.status(400).render("error", templateVars);
+  }
+  urlDatabase[shortURL].longURL = longURL;
   const templateVars = {
     user: users[userID],
-    shortURL: req.params.shortURL,
-    longURL: req.body.longURL
+    shortURL: shortURL,
+    longURL: longURL
   };
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  //res.send("Hello!");
-  res.redirect("/urls");
-});
+// GET Endpoints
 
-// temporary endpoint to visually view the current urlDatabase
+//temporary endpoint to visually view the current urlDatabase for debugging purposes
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// temporary endpoint to visually view the current users
-app.get("/users.json", (req, res) => {
-  res.json(users);
+// temporary endpoint to visually view the current users for debugging purposes
+// app.get("/users.json", (req, res) => {
+//   res.json(users);
+// });
+
+app.get("/", (req, res) => {
+  res.redirect("/urls");
 });
 
+// main page which displays urls if logged into a user with urls and a message stating that the viewer needs to registe/login or create new urls
 app.get("/urls", (req, res) => {
   const userID = req.session.user_id;
   const currentUserURLS = urlsForUser(userID, urlDatabase);
@@ -177,6 +208,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// registration page for users to create a new account
 app.get("/register", (req, res) => {
   const userID = req.session.user_id;
   const templateVars = {
@@ -186,6 +218,7 @@ app.get("/register", (req, res) => {
   res.render("register", templateVars);
 });
 
+// login page for users to sign into an existing account
 app.get("/login", (req, res) => {
   const userID = req.session.user_id;
   const templateVars = {
@@ -195,11 +228,12 @@ app.get("/login", (req, res) => {
   res.render("login", templateVars);
 });
 
+// page that will allow signed in users to create new urls
 app.get("/urls/new", (req, res) => {
   const userID = req.session.user_id;
+  // if not signed in, send to the login page
   if (!users[userID]) {
-    res.redirect("/login");
-    return;
+    return res.redirect("/login");
   }
   const templateVars = {
     user: users[userID],
@@ -207,45 +241,57 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+// this endpoint will redirect users to the long URLs that they have associated with the specific short URL
 app.get("/u/:shortURL", (req, res) => {
+  const userID = req.session.user_id;
+  // if the shortURL does not exist, send to the error page
+  if (!urlDatabase[req.params.shortURL]) {
+    const templateVars = {
+      user: users[userID],
+      error: "400 - This is not a valid link!"
+    };
+    return res.status(400).render("error", templateVars);
+  }
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
+// this page will send users to the short URL detail page where they can edit the long URL
 app.get("/urls/:shortURL", (req, res) => {
   const userID = req.session.user_id;
+  // if the shortURL does not exist, send to the error page
   if (!urlDatabase[req.params.shortURL]) {
     const templateVars = {
       user: users[userID],
-      error: "This is not a valid link!"
+      error: "400 - This is not a valid link!"
     };
-    return res.render("error", templateVars);
+    return res.status(400).render("error", templateVars);
   }
-  if (urlDatabase[req.params.shortURL].userID !== userID) {
-    //return res.status(400).send("This is not your link!  Please sign in to the proper account to access this link!");
+  const shortURL = req.params.shortURL;
+  // if the shortURL does not belong to the current logged in user, send to error page with Unauthorized message
+  if (urlDatabase[shortURL].userID !== userID) {
     const templateVars = {
       user: users[userID],
-      error: "This is not your link!  Please sign in to the proper account to access this link!"
+      error: "403 - Unauthorized access to this link!  Please sign into the proper account to access this link!"
     };
-    return res.render("error", templateVars);
+    return res.status(403).render("error", templateVars);
   }
   const templateVars = {
     user: users[userID],
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL].longURL
+    shortURL: shortURL,
+    longURL: urlDatabase[shortURL].longURL
   };
   res.render("urls_show", templateVars);
 });
 
-// app.get("/error", (req, res) => {
-//   const userID = req.session.user_id;
-//   const error = req.params.error;
-//   const templateVars = {
-//     user: users[userID],
-//     error: error
-//   }
-//   res.render("error", templateVars);
-// });
+// handles pages that are requested but do not exist
+app.get('*', function(req, res) {
+  const templateVars = {
+    user: users[req.session.user_id],
+    error: "404 - This page does not exist!  Please click one of the links above!"
+  };
+  return res.status(404).render("error", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
